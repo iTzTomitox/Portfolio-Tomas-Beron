b162bae4ef328996c3eefb9bc6aeec498f2c8a5d
Tomas Beron <wikipediatomitob@gmail.com>
2024-11-06 17:22:30 -0300

    menu

--- script/formulario.js
+++ script/formulario.js
@@ -25,3 +25,36 @@ function toggleMenu() {
   const navLinks = document.querySelector('.nav-links');
   navLinks.classList.toggle('open');
 }
+
+const menu = document.querySelector('.nav-links');
+const menuButton = document.querySelector('.hamburger-menu');
+
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('open');
+});
+
+// Cerrar el menú cuando se hace clic en un enlace del menú
+const navLinks = document.querySelectorAll('.nav-links a');
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('open');
+  });
+});
+
+// Cerrar el menú al hacer clic fuera de él
+document.addEventListener('click', (event) => {
+  if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
+    menu.classList.remove('open');
+  }
+});
+
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  const body = document.querySelector('body');
+  
+  // Cambiar la clase 'open' para mostrar o esconder el menú
+  navLinks.classList.toggle('open');
+  
+  // Añadir o quitar la clase 'menu-open' al body para evitar que el contenido se desplace
+  body.classList.toggle('menu-open');
+}
